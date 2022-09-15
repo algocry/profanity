353d686944f7e543c77d4f4246ce61d52986c0a8
0x0is1 <42772562+0x0is1@users.noreply.github.com>
2022-09-15 13:27:30 +0530

    [-] Removed unused authentication

--- libs/parseOAS.js
+++ libs/parseOAS.js
@@ -2,59 +2,58 @@ const fetch = require('node-fetch');
 eval(require('fs').readFileSync('./libs/encryptHandler.js', 'utf8'));
 const BASEURL = 'https://oas.lpu.in/api/OnlineExam';
 
-const headers = (lact, act) => {
+const headers = () => {
     return {
         "content-type": "application/json; charset=utf-8",
         "x-requested-with": "XMLHttpRequest",
-        "cookie": `lpu_token={\"lpu_access_token\":\"${lact}\"}; token={\"access_token\":\"${act}\"};`,
         "Referer": BASEURL,
     }
 };
 
-async function fetch_attempted(reg, lact, act) {
+async function fetch_attempted(reg) {
     var url = `${BASEURL}/GetAttemptedTestForStudent?LoginId=${reg}`;
     var data = await fetch(url, {
-        "headers": headers(lact, act),
+        "headers": headers(),
         "method": "GET"
     });
     var resp = await data.json();
     return resp;
 }
 
-async function fetch_result(tid, reg, lact, act) {
+async function fetch_result(tid, reg) {
     var url = `${BASEURL}/GetTestDetailForResultAnalysis?EncTestId=${encodeURI(encd(tid))}&LoginId=${encodeURI(encd(reg))}`;
     var data = await fetch(url, {
-        "headers": headers(lact, act),
+        "headers": headers(),
         "method": "GET"
     });
     var resp = await data.json();
     return resp;
 }
 
-async function fetch_username(reg, lact, act) {
+async function fetch_username(reg) {
     var url = `${BASEURL}/GetUserName?LoginId=${encodeURI(encd(reg))}`;
     var data = await fetch(url, {
-        "headers": headers(lact, act),
+        "headers": headers(),
         "method": "GET"
     });
     var resp = await data.json();
     return resp;
 }
 
-async function fetch_questions(lact, act, q_no) {
+async function fetch_questions(q_no) {
     var url = `${BASEURL}/GetQuestionDetailToDisplay?QuestionId=${q_no}`;
     var data = await fetch(url, {
-        "headers": headers(lact, act),
+        "headers": headers(),
         "method": "GET"
     });
     var resp = await data.json();
     return resp;
 }
 
-async function fetch_options(lact, act, q_no) {
+async function fetch_options(q_no) {
     var url = `${BASEURL}/GetOptionDetailToDisplay?QuestionId=${q_no}`;
     var data = await fetch(url, {
-        "headers": headers(lact, act),
+        "headers": headers(),
         "method": "GET"
     });
     var resp = await data.json();
