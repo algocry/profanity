690d1be1d544f2c41949bf1bd40614fd763312ca
Suryaa <0x0is1@protonmail.com>
2022-11-16 11:36:15 +0530

    [+] Added git auto install command

--- update.bat
+++ update.bat
@@ -1,7 +1,8 @@
 @echo off
 WHERE git
 if %ERRORLEVEL% NEQ 0 (
-    ECHO Git not found
+    echo Git not found! Installing...
+    winget install -e --id Git.Git
 ) else (
     echo Git found
     git pull
